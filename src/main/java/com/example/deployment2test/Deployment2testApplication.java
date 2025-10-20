a18ddd5fc899bc1904bf285d8c124ae58ef2db14
Arfeen2025 <arfeenmomin2@gmail.com>
2025-10-20 12:01:29 +0530

    initial

---
+++ src/main/java/com/example/deployment2test/Deployment2testApplication.java
@@ -0,0 +1,13 @@
+package com.example.deployment2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deployment2testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deployment2testApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/deployment2test/Deployment2testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deployment2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deployment2testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
